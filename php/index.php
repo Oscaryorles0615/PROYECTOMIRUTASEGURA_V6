d319f1f8fcaa916405fd8df859c56fd8acf40a48
Oscaryorles0615 <iosorio0615@gmail.com>
2025-03-16 19:43:13 -0500

    creacion del codigo php

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    $servidor = "locallhost";
+    $usuario = "root";
+    $clave = "";
+    $baseDeDatos = "MI_RUTA_SEGURA";
+
+    $enlace = mysqli_connect ($servidor, $usuario, $clave, $baseDeDatos);
